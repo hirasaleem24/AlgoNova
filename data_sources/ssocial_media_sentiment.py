eb7b1f64893f6da3350e6e65546440132d7de91f
hirasaleem24 <hirasaleem24@outlook.com>
2025-10-31 13:58:32 +0400

    Delete data_sources directory

--- data_sources/ssocial_media_sentiment.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-def analyze_social_media(posts):
-    sentiment_analyzer = pipeline("sentiment-analysis")
-    results = sentiment_analyzer(posts)
-    return results
