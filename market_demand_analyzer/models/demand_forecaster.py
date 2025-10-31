b9804f8f674745cf3500c23780e8b5cf8f0b3273
hirasaleem24 <hirasaleem24@outlook.com>
2025-10-31 13:50:43 +0400

    Update demand_forecaster.py

--- market_demand_analyzer/models/demand_forecaster.py
+++ market_demand_analyzer/models/demand_forecaster.py
@@ -1,1 +1,9 @@
+from sklearn.linear_model import LinearRegression
+import pandas as pd
 
+def predict_market_demand(data: pd.DataFrame):
+    model = LinearRegression()
+    X = data[["feature_usage", "search_volume"]]
+    y = data["market_revenue"]
+    model.fit(X, y)
+    return model.predict(X[-1:].values)[0]
